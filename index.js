b9a014dc67eafcaa7b50ffdfe92466f72a8d59f9
callmeradical <lars@paniksrvr.info>
2017-04-20 14:59:01 -0400

    default env variables

--- index.js
+++ index.js
@@ -89,6 +89,8 @@ function cli() {
       //'add': 'a'
     },
     default: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       json: false,
       stats: false,
       add: []
